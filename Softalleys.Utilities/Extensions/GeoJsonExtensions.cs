02d936f7b16346cf93b7206e6ec0cff3f96b4974
MicroMatt27170 <matthew.montesdeoca@outlook.com>
2025-03-15 16:05:13 -0600

    wip

---
+++ Softalleys.Utilities/Extensions/GeoJsonExtensions.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Spatial;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+
+namespace Softalleys.Utilities.Extensions;
+
+public static class GeoJsonExtensions
+{
+    public static Point ToNetTopologyPoint(this GeographyPoint geographyPoint)
+    {
+        return new Point(geographyPoint.Longitude, geographyPoint.Latitude) { SRID = 4326 };
+    }
+    
+    public static GeographyPoint ToGeographyPoint(this Point point)
+    {
+        return GeographyPoint.Create(point.Y, point.X);
+    }
+    
+    public static GeometryPoint ToGeometryPoint(this Point geographyPoint)
+    {
+        return GeometryPoint.Create(geographyPoint.X, geographyPoint.Y);
+    }
+    
+    public static byte[] ToWellKnownBinary(this Point point)
+    {
+        return point.AsBinary();
+    }
+    
+    public static Point FromWellKnownBinaryToPoint(this byte[] wkb)
+    {
+        var reader = new WKBReader();
+        NetTopologySuite.Geometries.Geometry geometry = reader.Read(wkb);
+        
+        if (geometry is Point point)
+        {
+            return point;
+        }
+        
+        throw new ArgumentException("The provided WKB does not represent a Point geometry.");
+    }
+    
+    public static Polygon FromWellKnownBinaryToPolygon(this byte[] wkb)
+    {
+        var reader = new WKBReader();
+        NetTopologySuite.Geometries.Geometry geometry = reader.Read(wkb);
+        
+        if (geometry is Polygon polygon)
+        {
+            return polygon;
+        }
+        
+        throw new ArgumentException("The provided WKB does not represent a Polygon geometry.");
+    }
+
+    public static LineString FromWellKnownBinaryToLineString(this byte[] wkb)
+    {
+        var reader = new WKBReader();
+        NetTopologySuite.Geometries.Geometry geometry = reader.Read(wkb);
+        
+        if (geometry is LineString lineString)
+        {
+            return lineString;
+        }
+        
+        throw new ArgumentException("The provided WKB does not represent a LineString geometry.");
+    }
+    
+    public static NetTopologySuite.Geometries.Geometry FromWellKnownBinaryToGeometry(this byte[] wkb)
+    {
+        var reader = new WKBReader();
+        return reader.Read(wkb);
+    }
+    
+    public static NetTopologySuite.Geometries.Polygon ToNetTopologyPolygon(this GeographyPolygon geographyPolygon)
+    {
+        var linearRings = geographyPolygon.Rings.Select(ring => 
+                new LinearRing(ring.Points.Select(x => 
+                        new Coordinate(x.Longitude, x.Latitude))
+                    .ToArray()))
+            .ToList();
+
+        var polygon = new Polygon(linearRings.First());
+
+        return polygon;
+    }
+    
+    public static GeographyPolygon ToGeographyPolygon(this NetTopologySuite.Geometries.Polygon polygon)
+    {
+        var factory = GeographyFactory.Polygon(CoordinateSystem.Geography(null));
+
+        factory.Ring(polygon.Coordinate.Y, polygon.Coordinate.X);
+
+        foreach (var c in polygon.Coordinates)
+        {
+            factory.LineTo(c.Y, c.X);
+        }
+
+        return factory.Build();
+    }
+    
+    
+}
---
+++ Softalleys.Utilities/Extensions/UlidExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace Softalleys.Utilities.Extensions;
+
+/// <summary>
+///     Provides extension methods for converting ULID strings to <see cref="DateTimeOffset"/> values 
+///     and for creating ULIDs from <see cref="DateTimeOffset"/> values.
+/// </summary>
+public static class UlidExtensions
+{
+    /// <summary>
+    ///     Converts a ULID string to a <see cref="DateTimeOffset"/> representing the time portion of the ULID.
+    /// </summary>
+    /// <param name="ulid">
+    ///     The ULID string to convert.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="DateTimeOffset"/> corresponding to the ULID's time, or <c>null</c> if parsing fails.
+    /// </returns>
+    public static DateTimeOffset? FromUlid(this string ulid)
+    {
+        if (Ulid.TryParse(ulid, out var parsedUlid))
+        {
+            return parsedUlid.Time;
+        }
+        
+        return null;
+    }
+    
+    /// <summary>
+    ///     Converts a <see cref="DateTimeOffset"/> to a ULID string.
+    /// </summary>
+    /// <param name="dateTimeOffset">
+    ///     The <see cref="DateTimeOffset"/> to convert.
+    /// </param>
+    /// <returns>
+    ///     A ULID string generated from the specified <see cref="DateTimeOffset"/>.
+    /// </returns>
+    public static string ToUlidString(this DateTimeOffset dateTimeOffset)
+    {
+        var ulid = Ulid.NewUlid(dateTimeOffset);
+        return ulid.ToString();
+    }
+
+    /// <summary>
+    ///     Converts a <see cref="DateTimeOffset"/> to a <see cref="Ulid"/> instance.
+    /// </summary>
+    /// <param name="dateTimeOffset">
+    ///     The <see cref="DateTimeOffset"/> from which to generate the ULID.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="Ulid"/> generated from the specified <see cref="DateTimeOffset"/>.
+    /// </returns>
+    public static Ulid ToUlid(this DateTimeOffset dateTimeOffset)
+    {
+        return Ulid.NewUlid(dateTimeOffset);
+    }
+}
