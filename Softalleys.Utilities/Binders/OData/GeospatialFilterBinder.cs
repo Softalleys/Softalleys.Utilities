2ffd545ec2916212696e3f771acda4f73ac5b0e6
MicroMatt27170 <matthew.montesdeoca@outlook.com>
2025-03-16 22:37:44 -0600

    docs

--- Softalleys.Utilities/Binders/OData/GeospatialFilterBinder.cs
+++ Softalleys.Utilities/Binders/OData/GeospatialFilterBinder.cs
@@ -9,20 +9,25 @@ using Geometry = NetTopologySuite.Geometries.Geometry;
 
 namespace Softalleys.Utilities.Binders.OData;
 
+/// <summary>
+/// Custom filter binder for handling geospatial functions in OData queries.
+/// </summary>
 public class GeospatialFilterBinder : FilterBinder
 {
     private const string GeoDistanceFunctionName = "geo.distance";
-
     private const string GeoIntersectsFunctionName = "geo.intersects";
-
     private const string GeoContainsFunctionName = "geo.contains";
 
     private static readonly MethodInfo DistanceMethodDb = typeof(Geometry).GetMethod(nameof(Geometry.Distance))!;
-
     private static readonly MethodInfo IntersectsMethodDb = typeof(Geometry).GetMethod(nameof(Geometry.Intersects))!;
-
     private static readonly MethodInfo ContainsMethodDb = typeof(Geometry).GetMethod(nameof(Geometry.Contains))!;
 
+    /// <summary>
+    /// Binds a single value function call node to an expression.
+    /// </summary>
+    /// <param name="node">The single value function call node.</param>
+    /// <param name="context">The query binder context.</param>
+    /// <returns>The bound expression.</returns>
     public override Expression? BindSingleValueFunctionCallNode(SingleValueFunctionCallNode node,
         QueryBinderContext context)
     {
@@ -35,6 +40,12 @@ public class GeospatialFilterBinder : FilterBinder
         };
     }
 
+    /// <summary>
+    /// Binds the geo.distance function call node to an expression.
+    /// </summary>
+    /// <param name="node">The single value function call node.</param>
+    /// <param name="context">The query binder context.</param>
+    /// <returns>The bound expression.</returns>
     private Expression? BindGeoDistance(SingleValueFunctionCallNode node, QueryBinderContext context)
     {
         Expression[] arguments = BindArguments(node.Parameters, context);
@@ -51,12 +62,17 @@ public class GeospatialFilterBinder : FilterBinder
 
         GetPointExpressions(arguments, propertyName, out var memberExpression, out var constantExpression);
         if (constantExpression == null) return null;
-        
+
         var ex = Expression.Call(memberExpression, DistanceMethodDb, constantExpression);
         return ex;
-
     }
 
+    /// <summary>
+    /// Binds the geo.intersects function call node to an expression.
+    /// </summary>
+    /// <param name="node">The single value function call node.</param>
+    /// <param name="context">The query binder context.</param>
+    /// <returns>The bound expression.</returns>
     private Expression? BindGeoIntersects(SingleValueFunctionCallNode node, QueryBinderContext context)
     {
         Expression[] arguments = BindArguments(node.Parameters, context);
@@ -87,9 +103,14 @@ public class GeospatialFilterBinder : FilterBinder
         var ex = Expression.Call(memberExpression, IntersectsMethodDb, constantExpression);
 
         return ex;
-
     }
 
+    /// <summary>
+    /// Binds the geo.contains function call node to an expression.
+    /// </summary>
+    /// <param name="node">The single value function call node.</param>
+    /// <param name="context">The query binder context.</param>
+    /// <returns>The bound expression.</returns>
     private Expression? BindGeoContains(SingleValueFunctionCallNode node, QueryBinderContext context)
     {
         Expression[] arguments = BindArguments(node.Parameters, context);
@@ -104,14 +125,20 @@ public class GeospatialFilterBinder : FilterBinder
         }
 
         GetPointExpressions(arguments, propertyName, out var memberExpression, out var constantExpression);
-        
+
         if (constantExpression == null) return null;
         var ex = Expression.Call(memberExpression, ContainsMethodDb, constantExpression);
 
         return ex;
-
     }
 
+    /// <summary>
+    /// Gets the member and constant expressions for the given arguments and property name.
+    /// </summary>
+    /// <param name="expressions">The expressions.</param>
+    /// <param name="propertyName">The property name.</param>
+    /// <param name="memberExpression">The member expression.</param>
+    /// <param name="constantExpression">The constant expression.</param>
     private static void GetPointExpressions(Expression[] expressions, string? propertyName,
         out MemberExpression? memberExpression, out ConstantExpression? constantExpression)
     {
@@ -128,12 +155,12 @@ public class GeospatialFilterBinder : FilterBinder
                     // expression of the property of type NetTopologySuite.Geometries.Geometry
                     // We can get the NetTopologySuite.Geometries.Geometry property from propertyName and 
                     // the model of the entity
-                    
-                    
+
                     memberExpression = Expression.Property(memberExpr.Expression!, "Location");
-                    
+
                     continue;
                 }
+
                 var geography = GetGeographyFromConstantExpression(constantExpr);
 
                 constantExpression = geography switch
@@ -150,6 +177,11 @@ public class GeospatialFilterBinder : FilterBinder
         }
     }
 
+    /// <summary>
+    /// Gets the geography object from the constant expression.
+    /// </summary>
+    /// <param name="expression">The constant expression.</param>
+    /// <returns>The geography object.</returns>
     private static Geography? GetGeographyFromConstantExpression(ConstantExpression? expression)
     {
         if (expression == null) return null;
@@ -157,6 +189,11 @@ public class GeospatialFilterBinder : FilterBinder
         return constantExpressionValuePropertyInfo?.GetValue(expression.Value) as Geography;
     }
 
+    /// <summary>
+    /// Creates a polygon from the given geography polygon.
+    /// </summary>
+    /// <param name="geographyPolygon">The geography polygon.</param>
+    /// <returns>The created polygon.</returns>
     private static Polygon CreatePolygon(GeographyPolygon geographyPolygon)
     {
         var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
@@ -169,6 +206,12 @@ public class GeospatialFilterBinder : FilterBinder
         return geometryFactory.CreatePolygon(linearRing);
     }
 
+    /// <summary>
+    /// Creates a point from the given latitude and longitude.
+    /// </summary>
+    /// <param name="latitude">The latitude.</param>
+    /// <param name="longitude">The longitude.</param>
+    /// <returns>The created point.</returns>
     private static Point CreatePoint(double latitude, double longitude)
     {
         // 4326 is the most common coordinate system used by GPS/Maps
